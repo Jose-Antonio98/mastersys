50234ecfb932de7c284e56697e0b9c057b363c29
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-08-01 20:29:37 -0300

    Feat(dto) criar DTOs de aluno

--- src/main/java/dev/jose/mastersys/dto/AlunoRequest.java
+++ src/main/java/dev/jose/mastersys/dto/AlunoRequest.java
@@ -1,4 +1,47 @@
 package dev.jose.mastersys.dto;
 
-public class AlunoRequest {
+import dev.jose.mastersys.domain.Aluno;
+
+import java.time.LocalDate;
+
+public record AlunoRequest(
+        String nome,
+        LocalDate dataNascimento,
+        String sexo,
+        String telefone,
+        String celular,
+        String email,
+        String observacao,
+        String endereco,
+        String numero,
+        String complemento,
+        String bairro,
+        String cidade,
+        String estado,
+        String cep
+
+) {
+
+    public Aluno toEntity() {
+        Aluno aluno = new Aluno();
+         preencher(aluno);
+         return aluno;
+    }
+
+    public void preencher (Aluno aluno) {
+        aluno.setNome(nome);
+        aluno.setDataNascimento(dataNascimento);
+        aluno.setSexo(sexo);
+        aluno.setTelefone(telefone);
+        aluno.setCelular(celular);
+        aluno.setEmail(email);
+        aluno.setObservacao(observacao);
+        aluno.setEndereco(endereco);
+        aluno.setNumero(numero);
+        aluno.setComplemento(complemento);
+        aluno.setBairro(bairro);
+        aluno.setCidade(cidade);
+        aluno.setEstado(estado);
+        aluno.setCep(cep);
+    }
 }
