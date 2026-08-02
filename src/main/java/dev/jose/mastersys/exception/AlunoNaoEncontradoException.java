15dd4ecc82cd00e14978ccdf1124a6768de2a43f
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-08-02 18:38:52 -0300

    refactor(exception) Melhorar a mensagem de retorno

--- src/main/java/dev/jose/mastersys/exception/AlunoNaoEncontradoException.java
+++ src/main/java/dev/jose/mastersys/exception/AlunoNaoEncontradoException.java
@@ -2,7 +2,7 @@ package dev.jose.mastersys.exception;
 
 public class AlunoNaoEncontradoException extends RuntimeException{
 
-    public AlunoNaoEncontradoException(){
-        super("Aluno não encontrado");
+    public AlunoNaoEncontradoException(Long id) {
+        super("Aluno não encontrado. Id: " + id);
     }
 }
