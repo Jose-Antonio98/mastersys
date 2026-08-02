d40fd28906eaaa2383f7b6f7c84a0746ddc4a1b0
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-08-02 18:41:29 -0300

    Feat(exception) adicionar tratamento de excessões

---
+++ src/main/java/dev/jose/mastersys/exception/GlobalExceptionHandler.java
@@ -0,0 +1,64 @@
+package dev.jose.mastersys.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResponse> tratarErroValidacao(MethodArgumentNotValidException ex) {
+        List<String> mensagens = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
+                .toList();
+
+       return criarResposta(HttpStatus.BAD_REQUEST, "Erro de validação", mensagens);
+    }
+
+    @ExceptionHandler(EmailJaCadastradoException.class)
+    public ResponseEntity<ErroResponse> tratarEmailJaCadastrado(
+            EmailJaCadastradoException ex) {
+
+        return criarResposta(HttpStatus.CONFLICT, "Conflito", List.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AlunoNaoEncontradoException.class)
+    public ResponseEntity<ErroResponse> tratarAlunoNaoEncontrado(
+            AlunoNaoEncontradoException ex){
+
+        return criarResposta(HttpStatus.NOT_FOUND,"Recurso não encontrado", List.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErroResponse> tratarErroGenerico(Exception ex) {
+
+        return criarResposta(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Erro interno do servidor",
+                List.of("Ocorreu um erro inesperado.")
+        );
+    }
+
+    private ResponseEntity<ErroResponse> criarResposta(
+            HttpStatus status,
+            String titulo,
+            List<String> mensagens) {
+
+        ErroResponse response = new ErroResponse(
+                LocalDateTime.now(),
+                status.value(),
+                titulo,
+                mensagens
+        );
+
+        return ResponseEntity.status(status).body(response);
+    }
+}
