42c437d669d467b5ee63c643f9e7e9eb90bdc3ae
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-08-02 18:40:17 -0300

    Feat(AlunoSpecification) adicionar consulta por filtros

--- src/main/java/dev/jose/mastersys/service/AlunoService.java
+++ src/main/java/dev/jose/mastersys/service/AlunoService.java
@@ -1,12 +1,14 @@
 package dev.jose.mastersys.service;
 
 import dev.jose.mastersys.domain.Aluno;
+import dev.jose.mastersys.dto.AlunoFiltroRequest;
 import dev.jose.mastersys.dto.AlunoRequest;
 import dev.jose.mastersys.dto.AlunoResponse;
 import dev.jose.mastersys.exception.AlunoNaoEncontradoException;
 import dev.jose.mastersys.exception.EmailJaCadastradoException;
 import dev.jose.mastersys.repository.AlunoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import dev.jose.mastersys.specification.AlunoSpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class AlunoService {
 
-    @Autowired
-    private AlunoRepository alunoRepository;
+    private final AlunoRepository alunoRepository;
 
+    public AlunoService(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
 
     public AlunoResponse cadastrar(AlunoRequest request){
         if (request.email() != null && alunoRepository.existsByEmail(request.email())) {
@@ -26,26 +30,27 @@ public class AlunoService {
         return AlunoResponse.fromEntity(alunoRepository.save(request.toEntity()));
     }
 
-    public Page<AlunoResponse> listar(Pageable pageable){
-        return alunoRepository.findAll(pageable).map(AlunoResponse::fromEntity);
+    public Page<AlunoResponse> listar(AlunoFiltroRequest filtro, Pageable pageable){
+        return alunoRepository.findAll(AlunoSpecification.filtros(filtro), pageable)
+                .map(AlunoResponse::fromEntity);
     }
 
     public AlunoResponse buscarPorId(Long id){
         return AlunoResponse.fromEntity(buscarEntityPorId(id));
     }
 
-    public AlunoResponse atualizar(long id, AlunoRequest request){
+    public AlunoResponse atualizar(Long id, AlunoRequest request){
         Aluno aluno = buscarEntityPorId(id);
         request.preencher(aluno);
-        return AlunoResponse.fromEntity(alunoRepository.save(request.toEntity()));
+        return AlunoResponse.fromEntity(alunoRepository.save(aluno));
     }
 
-    public void excluir(long id){
+    public void excluir(Long id){
         Aluno aluno = buscarEntityPorId(id);
         alunoRepository.delete(aluno);
     }
 
     private Aluno buscarEntityPorId(Long id){
-        return alunoRepository.findById(id).orElseThrow(AlunoNaoEncontradoException::new);
+        return alunoRepository.findById(id).orElseThrow(() -> new AlunoNaoEncontradoException(id));
     }
 }
---
+++ src/main/java/dev/jose/mastersys/specification/AlunoSpecification.java
@@ -0,0 +1,83 @@
+package dev.jose.mastersys.specification;
+
+import dev.jose.mastersys.domain.Aluno;
+import dev.jose.mastersys.dto.AlunoFiltroRequest;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.text.Normalizer;
+
+
+public class AlunoSpecification {
+
+    public static Specification<Aluno> filtros(AlunoFiltroRequest filtro) {
+        return Specification.where(nomeContem(filtro.nome()))
+                .and(emailContem(filtro.email()))
+                .and(celularContem(filtro.celular()))
+                .and(cidadeContem(filtro.cidade()))
+                .and(estadoIgual(filtro.estado()));
+    }
+
+    private static Specification<Aluno> nomeContem(String nome) {
+        return (root, query, criteriaBuilder) -> {
+            if (nome == null || nome.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.like(criteriaBuilder.function("unaccent",
+                    String.class,criteriaBuilder.lower(root.get("nome"))),
+                    "%" + removerAcentos(nome.toLowerCase()) + "%");
+
+        };
+    }
+
+    private static Specification<Aluno> emailContem(String email) {
+        return (root, query, criteriaBuilder) -> {
+            if (email == null || email.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get("email")),
+                    "%" + email.toLowerCase() + "%");
+        };
+    }
+
+    private static Specification<Aluno> celularContem(String celular) {
+        return (root, query, criteriaBuilder) -> {
+            if (celular == null || celular.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get("celular")),
+                    "%" + celular.toLowerCase() + "%");
+        };
+    }
+
+    private static Specification<Aluno> cidadeContem(String cidade) {
+        return (root, query, criteriaBuilder) -> {
+            if (cidade == null || cidade.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.like(criteriaBuilder.function("unaccent",
+                            String.class,criteriaBuilder.lower(root.get("cidade"))),
+                    "%" + removerAcentos(cidade.toLowerCase()) + "%");
+
+        };
+    }
+
+    private static Specification<Aluno> estadoIgual(String estado) {
+        return (root, query, criteriaBuilder) -> {
+            if (estado == null || estado.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.equal(criteriaBuilder.upper(root.get("estado")),
+                    estado.toUpperCase());
+        };
+    }
+
+    private static String removerAcentos(String texto) {
+        return Normalizer.normalize(texto, Normalizer.Form.NFD)
+                .replaceAll("\\p{M}", "");
+    }
+}
