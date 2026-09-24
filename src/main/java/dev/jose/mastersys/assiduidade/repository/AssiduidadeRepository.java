447a4220252db2952ab30f0ea514306488e4d1f7
Jose-Antonio98 <joseantoniochavesjunior@gmail.com>
2026-09-24 18:45:29 -0300

    refactor: corrigir nome de classes e mudança no escopo do projeto.

--- src/main/java/dev/jose/mastersys/assiduidade/repository/AssiduidadeRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.jose.mastersys.assiduidade.repository;
-
-import dev.jose.mastersys.assiduidade.domain.Assiduidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssiduidadeRepository extends JpaRepository<Assiduidade, Long> {
-}
